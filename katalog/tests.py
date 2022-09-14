29f37aef45b59235cb329b7cfb3f80779a903e58
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-14 10:11:40 +0700

    implementing testing in tests.py

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import show_katalog
 
 # Create your tests here.
+class URLTest(TestCase):
+    def test_url_katalog(self):
+        response = Client().get('/katalog/')
+        self.assertEqual(response.status_code, 200)
+    def test_template_katalog(self):
+        response = Client().get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
