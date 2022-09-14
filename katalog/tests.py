1131d6676ef314416605a7fb247d309ae5a88958
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-14 10:20:10 +0700

    delete testing

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,12 +1,3 @@
-from django.test import TestCase, Client
-from django.urls import resolve
-from .views import show_katalog
+from django.test import TestCase
 
 # Create your tests here.
-class URLTest(TestCase):
-    def test_url_katalog(self):
-        response = Client().get('/katalog/')
-        self.assertEqual(response.status_code, 200)
-    def test_template_katalog(self):
-        response = Client().get('/katalog/')
-        self.assertTemplateUsed(response, 'katalog.html')
