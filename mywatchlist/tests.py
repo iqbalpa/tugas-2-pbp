6c357c3f3cc79ecdfa5173c9d36b93ad01d9cc3d
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-16 20:35:55 +0700

    Tugas 3: implement test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class MyWatchListTest(TestCase):
+    def test_url_html(self):
+        res = Client().get('/mywatchlist/html/')
+        self.assertEqual(res.status_code, 200)
+    def test_url_json(self):
+        res = Client().get('/mywatchlist/json/')
+        self.assertEqual(res.status_code, 200)
+    def test_url_xml(self):
+        res = Client().get('/mywatchlist/xml/')
+        self.assertEqual(res.status_code, 200)
