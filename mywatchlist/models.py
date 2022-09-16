ec5a46c28ec72f5e268dae7488e0d9569485e8a2
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-16 13:42:10 +0700

    Tugas 3: implement the path for /mywatchlist/

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class MyWatchList(models.Model):
     watched = models.BooleanField() # true = sudah, false = belum
     title = models.CharField(max_length=255)
-    rating = models.IntegerField(min=1, max=5)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     release_date = models.DateTimeField()
     review = models.TextField()
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,7 @@
+# import httpresponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def show_mywatchlist(request):
+    return render(request, 'mywatchlist.html')
