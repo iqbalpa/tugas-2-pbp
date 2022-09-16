8c0ef99b3664c95d9cd1aee59e7cbd960a1ee39d
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-16 08:07:41 +0700

    Tugas 3: implement models.py

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class MyWatchList(models.Model):
+    watched = models.BooleanField() # true = sudah, false = belum
+    title = models.CharField(max_length=255)
+    rating = models.IntegerField(min=1, max=5)
+    release_date = models.DateTimeField()
+    review = models.TextField()
