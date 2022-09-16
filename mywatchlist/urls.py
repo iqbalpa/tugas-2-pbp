42f786b99ba85252da4178159a582e7d5fa633f0
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-16 19:50:47 +0700

    Tugas 3: implement path /json/ and /xml/

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import show_mywatchlist, show_mywatchlist_json, show_mywatchlist_xml
 
 urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
+    path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
+    path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,6 +1,8 @@
 # import httpresponse
 from django.shortcuts import render
 from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_mywatchlist(request):
@@ -9,3 +11,11 @@ def show_mywatchlist(request):
         "watchlist": mywatchlist
     }
     return render(request, 'mywatchlist.html', context)
+
+def show_mywatchlist_json(request):
+    mywatchlist = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", mywatchlist), content_type="application/json")
+
+def show_mywatchlist_xml(request):
+    mywatchlist = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", mywatchlist), content_type="application/xml")
