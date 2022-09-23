27e50fc3ea755fbdc9c8b43a6a4baf4bcf405bdc
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-23 17:26:23 +0700

    Tugas 4: implement all the necessary method in views.py and the html files

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from todolist.views import show_todolist, registrasi_user, login_user, logout_user
 
 urlpatterns = [
-    path('')
+    path('', show_todolist, name="show_todolist"),
+    path('registrasi/', registrasi_user, name="registrasi_user"),
+    path('login/', login_user, name="login_user"),
+    path('logout/', logout_user, name="logout_user"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,40 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def show_todolist(request):
+    username = None
+    if request.user.is_authenticated():
+        username = request.user.username
+    context = { "username":username }
+    return render(request, "todolist.html", context)
+
+def registrasi_user(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful")
+            return redirect("todolist:login")
+    context = {"form":form}
+    return render(request, "registration.html", context)
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("todolist:show_todolist")
+    context = {}
+    return render(request, "login.html", context)
+
+def logout_user(request):
+    logout(request)
+    return redirect("todolist:login")
