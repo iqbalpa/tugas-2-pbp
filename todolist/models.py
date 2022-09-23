b5d625560a3b6b643fcfd0e87814962b096c4b8d
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-23 16:37:23 +0700

    Tugas 4: implement task model in models.py

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,9 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    date = models.DateField(datetime.now())
+    title = models.CharField(max_length=150)
+    description = models.TextField()
